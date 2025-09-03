07c71ccd9a938523099f22a4852b87bad8b67f5d
Lucia Bolcatto <lucibolcatto11@gmail.com>
2025-09-03 17:34:29 -0300

    clase 3/9

--- typescript.ts
+++ typescript.ts
@@ -1,10 +1,9 @@
-// Typescript playground: https://www.typescriptlang.org/play 
-// console.log(): https://developer.mozilla.org/en-US/docs/Web/API/console/log_static 
+// Typescript playground: https://www.typescriptlang.org/play
+// console.log(): https://developer.mozilla.org/en-US/docs/Web/API/console/log_static
 // forEach: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-// filter(): https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter 
-// map(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map 
-// find(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find 
-
+// filter(): https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+// map(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+// find(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
 
 // LECTURAS:
 // https://www.w3schools.com/typescript/typescript_arrays.php
@@ -13,18 +12,14 @@
 // https://www.w3schools.com/typescript/typescript_union_types.php
 // https://www.w3schools.com/typescript/typescript_functions.php
 
-
-// Más práctica online: https://www.w3schools.com/typescript/exercise.php?filename=exercise_intro1 
-// Triple igual en comparaciones: https://developer.mozilla.org/es/docs/Web/JavaScript/Equality_comparisons_and_sameness 
-
+// Más práctica online: https://www.w3schools.com/typescript/exercise.php?filename=exercise_intro1
+// Triple igual en comparaciones: https://developer.mozilla.org/es/docs/Web/JavaScript/Equality_comparisons_and_sameness
 
 // Ejercicio 1
 // Declarar una variable de tipo string llamada "nombre" y asignarle tu nombre.
 // Declarar una variable de tipo number llamada "edad" y asignarle tu edad.
 // Declarar una variable de tipo boolean llamada "esEstudiante" y asignarle true o false.
 // Imprimir en consola el valor de cada variable usando console.log()
-
-
 
 // Ejercicio 2
 // Declarar una función que reciba dos números como parámetros y retorne su suma.
@@ -33,42 +28,33 @@
 // Realizar otra función que reste dos números.
 // Realizar otra función que multiplique dos números.
 
-
-
 // Ejercicio 3
 // Crear un array de números llamado "numeros" y agregar algunos valores.
 // Usa un bucle para imprimir cada número multiplicado por 2 (usando forEach())
-
-
 
 // Ejercicio 4
 // Declarar una interfaz "Persona" con las propiedades: nombre (string), edad (number), y esEstudiante (boolean).
 // Crear tres objetos que sigan la estructura de la interfaz.
 // Imprimir en consola.
 
-
-
 // Ejercicio 5
 // Declara una variable que pueda ser de tipo string o number.
 // Asigna un valor string y luego cambia su valor a number.
 // Imprimir en consola ambos casos.
 
-
-
 // Ejercicio 6
 // Definir una interfaz "Producto" con propiedades: nombre (string), precio (number), y enStock (boolean).
 // Crear un array de "productos" con seis objetos que sigan la interfaz.
-// Crear una función que reciba el array y retorne los productos en stock (sólo en stock). 
-// Crear otra función que reciba el array y retorne los productos sin stock (sólo sin stock). 
-// Se puede usar método filter() para esto. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter 
-
+// Crear una función que reciba el array y retorne los productos en stock (sólo en stock).
+// Crear otra función que reciba el array y retorne los productos sin stock (sólo sin stock).
+// Se puede usar método filter() para esto. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 
 // Ejercicio 7
 // Definir una interfaz "Cliente" con 4 propiedades a elección (por ahora solamente tipos string, booleanos y numbers).
 // Crear un array de "clientes" con seis objetos que sigan la interfaz.
-// Crear una función que reciba el array y retorne los clientes que cumplan una condición (segun el booleano que hayamos creado). 
-// Crear otra función que reciba el array y retorne los clientes que cumplan la condición inversa al punto de arriba (mismo booleano que hayamos elegido). 
-// Se puede usar método filter() para esto. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter 
+// Crear una función que reciba el array y retorne los clientes que cumplan una condición (segun el booleano que hayamos creado).
+// Crear otra función que reciba el array y retorne los clientes que cumplan la condición inversa al punto de arriba (mismo booleano que hayamos elegido).
+// Se puede usar método filter() para esto. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 
 /*
 
@@ -180,4 +166,34 @@ persons.forEach(logPerson);
 
 */
 
+// Ejercicio 1
+// Declarar una variable de tipo string llamada "nombre" y asignarle tu nombre.
+// Declarar una variable de tipo number llamada "edad" y asignarle tu edad.
+// Declarar una variable de tipo boolean llamada "esEstudiante" y asignarle true o false.
+// Imprimir en consola el valor de cada variable usando console.log()
 
+const nombre: string = 'Lucia';
+const edad: number = 23;
+const esEstudiante: boolean = true;
+console.log(nombre, edad, esEstudiante);
+
+// Ejercicio 2
+// Declarar una función que reciba dos números como parámetros y retorne su suma.
+// Asegurar que los parámetros y el valor de retorno sean de tipo number.
+// Llamar a la función e imprimir el resultado.
+// Realizar otra función que reste dos números.
+// Realizar otra función que multiplique dos números.
+
+console.log(suma(1, 2));
+
+function suma(a: number, b: number): number {
+  return a + b;
+}
+
+function resta(a: number, b: number): number {
+  return a - b;
+}
+
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
